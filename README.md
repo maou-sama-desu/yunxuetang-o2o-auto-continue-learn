173e23a8bee8ef6171d3b855c01d0328b0b90e00
魔王酱 <99876858+maou-sama-desu@users.noreply.github.com>
2024-08-16 19:28:34 +0800

    fix name typo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# yunxuetang-oto-script
+# yunxuetang-o2o-script
 
 云学堂企业培训自动继续学习脚本
 
--- yunxuetang-o2o-script.user.js
+++ yunxuetang-o2o-script.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        云学堂企业培训自动继续学习脚本
-// @namespace   https://github.com/maou-sama-desu/yunxuetang-oto-script
+// @namespace   https://github.com/maou-sama-desu/yunxuetang-o2o-script
 // @match       http*://*.yunxuetang.cn/o2o/*/playinfo*
 // @grant       none
-// @version     1.0.1
+// @version     2.0.0
 // @author      -
 // @license     MIT
 // @description 8/16/2024, 2:14:21 PM

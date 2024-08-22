bcb2d8115d2e2f0fe7390695659f7e39ca3b1129
魔王酱 <99876858+maou-sama-desu@users.noreply.github.com>
2024-08-22 21:12:49 +0800

    Update yunxuetang-o2o-script.user.js

--- yunxuetang-o2o-script.user.js
+++ yunxuetang-o2o-script.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name        云学堂企业培训自动继续学习脚本
+// @name        云学堂企业培训自动点击"继续学习"脚本
 // @namespace   https://github.com/maou-sama-desu/yunxuetang-o2o-script
 // @match       http*://*.yunxuetang.cn/o2o/*
 // @grant       none
-// @version     2.1.0
+// @version     2.2.0
 // @author      -
 // @license     MIT
 // @description 8/16/2024, 2:14:21 PM
@@ -14,6 +14,10 @@
 
     setInterval(function() {
         document.getElementsByClassName("yxtf-dialog__body")[0].getElementsByClassName("yxtf-button")[0].click();
-    }, 30000);
+    }, 1000);
+
+    setInterval(function() {
+        document.getElementsByClassName("yxtulcdsdk-course-player__back")[0].getElementsByClassName("yxtf-button")[0].click();
+    }, 1000);
 
 })();

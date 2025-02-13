bbccb6eb1ab9c07d9b4b58750c4a1fcc5b261c7a
魔王酱 <99876858+maou-sama-desu@users.noreply.github.com>
2025-02-14 02:05:37 +0800

    Update and rename yunxuetang-o2o-script.user.js to yunxuetang-o2o-auto-continue-learn.user.js

--- yunxuetang-o2o-auto-continue-learn.user.js
+++ yunxuetang-o2o-auto-continue-learn.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name        云学堂企业培训自动点击"继续学习"脚本
-// @namespace   https://github.com/maou-sama-desu/yunxuetang-o2o-script
+// @namespace   https://github.com/maou-sama-desu/yunxuetang-o2o-auto-continue-learn
 // @match       http*://*.yunxuetang.cn/o2o/*
 // @grant       none
-// @version     2.2.0
-// @author      -
+// @version     3.0.0
+// @author      github.com/maou-sama-desu
 // @license     MIT
-// @description 8/16/2024, 2:14:21 PM
 // ==/UserScript==
 
 (function() {
